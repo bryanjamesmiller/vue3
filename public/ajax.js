b6f74f9997c3f846754dc6c6d5072090fdeaae78
Bryan Miller <gratiafide@yahoo.com>
2015-12-31 19:45:02 -0600

    fixed json issue

--- public/ajax.js
+++ public/ajax.js
@@ -1,20 +1,22 @@
 Vue.component('tasks', {
     template: '#tasks-template',
-    data: function () {
+
+    data: function() {
         return {
             list: []
         };
     },
-    created: function(){
-        console.log('hello');
-var vm = this;
-        $.getJSON('/pretend-api', function(tasks){
-            console.log('hi');
-            console.log(tasks);
-            console.log('hi2');
-            vm.list = tasks;
 
-        }.bind(this));
+    created: function() {
+        this.getTaskListData();
+    },
+
+    methods: {
+        getTaskListData : function(){
+            $.getJSON("http://jsonapp.grat.xyz/pretend-api", function(json_result){
+                this.list = json_result;
+            }.bind(this));
+        }
     }
 });
 
--- resources/views/ajax.blade.php
+++ resources/views/ajax.blade.php
@@ -9,26 +9,20 @@
 <body>
 <div class="container">
     <h1>JSON task list reader</h1>
-    {{--
-    The $tasks variable gets converted to the "list" property (in main.js file where "props").
-    However, Laravel knows to encode the $tasks anyways so that json_encode isn't necessary.
-    Laravel knows to encode it because $tasks is a string without being encoded.
-    --}}
-
     <tasks></tasks>
-
 </div>
 
 <template id="tasks-template">
     <ul class="list-group">
-        {{-- "list" is then available here which got its value from $tasks above --}}
         <li class="list-group-item" v-for="task in list">
-            <span>@{{ task.activity }}</span><span>&nbsp;X</span>
+            @{{ task.activity }}
         </li>
     </ul>
 </template>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="http://cdnjs.cloudflare.com/ajax/libs/vue/1.0.13/vue.min.js"></script>
+
+
+<script src="http://code.jquery.com/jquery.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/vue/1.0.8/vue.min.js"></script>
 <script src="ajax.js"></script>
 </body>
 </html>
